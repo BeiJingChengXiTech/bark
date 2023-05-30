3a45d8dd1aaa24381a02da3c7652be52d329953a
jackgo <jackgo2080@gmail.com>
2023-05-30 13:31:58 +0800

    o

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import os
+from scipy.io.wavfile import write as write_wav
 
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
@@ -21,12 +22,6 @@ script = """
 Hey, have you heard about this new text-to-audio model called "Bark"? 
 Apparently, it's the most realistic and natural-sounding text-to-audio model 
 out there right now. People are saying it sounds just like a real person speaking. 
-I think it uses advanced machine learning algorithms to analyze and understand the 
-nuances of human speech, and then replicates those nuances in its own speech output. 
-It's pretty impressive, and I bet it could be used for things like audiobooks or podcasts. 
-In fact, I heard that some publishers are already starting to use Bark to create audiobooks. 
-It would be like having your own personal voiceover artist. I really think Bark is going to 
-be a game-changer in the world of text-to-audio technology.
 """.replace("\n", " ").strip()
 
 sentences = nltk.sent_tokenize(script)
@@ -39,5 +34,4 @@ for sentence in sentences:
     audio_array = generate_audio(sentence, history_prompt=SPEAKER)
     pieces += [audio_array, silence.copy()]
 
-
-Audio(np.concatenate(pieces), rate=SAMPLE_RATE).export("./out/test", format="wav")
+write_wav("./out/test.wav", SAMPLE_RATE, pieces)
