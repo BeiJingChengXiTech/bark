c3d5be599f2ae73220fac863c208613fcfb18702
jackgo <jackgo2080@gmail.com>
2023-05-31 19:57:56 +0800

    o

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ from bark import generate_audio, SAMPLE_RATE
 preload_models()
 
 script = """
-与循环神经网络（RNN）一样，Transformer模型旨在处理自然语言等顺序输入数据，可应用于翻译、文本摘要等任务。而与RNN不同的是，Transformer模型能够一次性处理所有输入数据。注意力机制可以为输入序列中的任意位置提供上下文。如果输入数据是自然语言，则Transformer不必像RNN一样一次只处理一个单词，这种架构允许更多的并行计算，并以此减少训练时间。
+Transformer模型于2017年由Google大脑的一个团队推出，现已逐步取代长短期记忆（LSTM）等RNN模型成为了NLP问题的首选模型。并行化优势允许其在更大的数据集上进行训练。这也促成了BERT、GPT等预训练模型的发展。这些系统使用了维基百科、Common Crawl等大型语料库进行训练，并可以针对特定任务进行微调
 """.replace("\n", " ").strip()
 
 sentences = nltk.sent_tokenize(script)
@@ -38,4 +38,4 @@ for sentence in sentences:
     # write pieces to wav file
     # format string "./out/test"+num+".wav"
 
-write_wav("./out/test2.wav", SAMPLE_RATE, piecesCombne)
+write_wav("./out/test3.wav", SAMPLE_RATE, piecesCombne)
